454f5e82b87580ff5186eea4c3e4b947bcf696ed
jieun kwon <midi4214@gmail.com>
2023-04-27 16:45:20 +0900

    4.27 컬렉션 문제 클래스 및 객체 생성

---
+++ src/jieun/com/ohgiraffers/hw1/controller/BookManager.java
@@ -0,0 +1,49 @@
+package jieun.com.ohgiraffers.hw1.controller;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import jieun.com.ohgiraffers.hw1.model.dto.BookDTO;
+
+public class BookManager {
+
+	private ArrayList <BookDTO> bookList;
+	private Scanner sc = new Scanner(System.in);
+	public BookManager() {
+		
+	}
+	
+	public void addBook(Book book) {
+		
+		
+	}
+	
+	public void deleteBook(int index) {
+		
+		
+		
+	}
+	
+	public void searchBook(String btitle) {
+		
+		
+		
+	}
+	
+	public void displayAll() {
+		
+		
+	}
+
+	public List<BookDTO> sortedBooklist(int type){
+		return bookList;
+		
+	}
+	
+	public void printBookList(List<BookDTO> printList){
+	
+}
+
+}
---
+++ src/jieun/com/ohgiraffers/hw1/view/BookMenu.java
@@ -0,0 +1,45 @@
+package jieun.com.ohgiraffers.hw1.view;
+
+import java.util.List;
+import java.util.Scanner;
+
+import jieun.com.ohgiraffers.hw1.controller.BookManager;
+import jieun.com.ohgiraffers.hw1.model.dto.BookDTO;
+
+public class BookMenu {
+ 
+	private Scanner sc = new Scanner(System.in);
+	private BookManager bm;
+		
+
+
+public BookMenu() {
+	
+}
+ 
+public void mainMenu() {
+	
+}
+
+public BookDTO inputBook() {
+	return null;
+
+}
+
+public String inputBookNo() {
+	return null;
+	
+}
+
+public String inputBookTitle() {
+	return null;
+	
+	
+}
+
+public List<BookDTO> selectSortedBook(){
+
+	return null;
+}
+
+}
